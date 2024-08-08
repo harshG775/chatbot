9ef547d80ee9b12fe727d424aee0bc17006684f8
Harsh <hgaur491@gmail.com>
2024-08-08 16:28:14 +0530

    completing project

---
+++ app.js
@@ -0,0 +1,52 @@
+import { logMessage } from "./modules/log.js";
+import { getChatbotResponse } from "./modules/chatbotAPI.js";
+import { speak } from "./modules/speech.js";
+
+const logContainer = document.getElementById("logContainer");
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+// Initialize speech recognition
+const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.interimResults = false;
+recognition.continuous = true;
+recognition.lang = "en-US";
+
+// Handle speech recognition results
+recognition.addEventListener("result", async (e) => {
+    try {
+        const text = e.results[e.results.length - 1][0].transcript.trim();
+        logMessage(logContainer, `user:${text}`, "user");
+        const response = await getChatbotResponse(text);
+        if (response) {
+            logMessage(logContainer, `Bot:${response}`, "bot");
+            // // Convert the response to speech
+            // speak(response);
+        } else {
+            console.log(" No response received.", "bot");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Add event listeners for start and stop buttons
+startBtn.addEventListener("click", () => {
+    recognition.start();
+    console.log("log", "Conversation started.");
+});
+stopBtn.addEventListener("click", () => {
+    recognition.stop();
+    console.log("log", "Conversation stopped.");
+});
+// Optional: Automatically restart recognition on end if required
+recognition.onend = () => {
+    recognition.start();
+    console.log("log", "Recognition ended.");
+};
+// Handle recognition errors
+recognition.onerror = (event) => {
+    console.log("log", `Error occurred in recognition: ${event.error}`);
+};
--- modules/log.js
+++ modules/log.js
@@ -1,5 +1,6 @@
-export function logMessage(log, message) {
+export function logMessage(log, message, className) {
     const p = document.createElement("p");
     p.textContent = message;
+    p.className = className;
     log.appendChild(p);
 }
